0808db5ebb4f32bac0cd1cdcdc0360b143cdfc2e
Loïc H. <loic@loicherman.ch>
2024-08-02 09:58:47 +0000

    fix: output user profiles

---
+++ terraform/_outputs.tf
@@ -0,0 +1,3 @@
+output "iam_users" {
+  value = module.iam.iam_users
+}
---
+++ terraform/modules/iam/_outputs.tf
@@ -0,0 +1,3 @@
+output "iam_users" {
+  value = { for key, val in aws_iam_user_login_profile.login: key => val.encrypted_password }
+}
