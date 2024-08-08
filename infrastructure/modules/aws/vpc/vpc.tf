52bb676914bdfc33318afe3e1a07e26ff12fe9b7
Loïc Herman <loic@loicherman.ch>
2024-08-08 16:56:44 +0200

    fix: add tags for elb to vpc

--- infrastructure/modules/aws/vpc/vpc.tf
+++ infrastructure/modules/aws/vpc/vpc.tf
@@ -15,7 +15,12 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
   private_subnet_tags = {
-    "karpenter.sh/discovery" = "presentium"
+    "karpenter.sh/discovery"          = "presentium"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
