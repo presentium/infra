40121e319ab4c8315cb93c8df5f7489ef71d263c
Christophe - CC4 <roulin43@gmail.com>
2024-08-22 21:30:04 +0200

    feat: registration pki and registration auth

---
+++ infrastructure/modules/vault/registration.tf
@@ -0,0 +1,33 @@
+resource "vault_pki_secret_backend_role" "role-registration" {
+  backend    = vault_mount.pki_int.path
+  issuer_ref = vault_pki_secret_backend_intermediate_set_signed.registration_intermediate.imported_issuers[0]
+
+  name = "registration-${var.domain}"
+
+  allowed_domains             = ["registration.${var.domain}"]
+  allow_bare_domains          = true
+  allow_subdomains            = false
+  allow_wildcard_certificates = false
+  allow_localhost             = false
+  allow_ip_sans               = false
+
+  key_type = "ed25519"
+
+  server_flag = false
+  client_flag = false
+
+  ou           = ["Registration"]
+  organization = [var.organization]
+  country      = [var.country]
+  max_ttl      = "8760h"
+  ttl          = "720h"
+}
+
+resource "vault_policy" "registration-policy" {
+  name   = "registration-${var.domain}"
+  policy = <<EOF
+    path "pki_int/issue/registration-${var.domain}" {
+      capabilities = [ "update" ]
+    }
+  EOF
+}
--- infrastructure/modules/vault/variables.tf
+++ infrastructure/modules/vault/variables.tf
@@ -11,3 +11,10 @@ variable "country" {
   description = "Country Code"
   default     = "CH"
 }
+
+variable "oidc_client_id" {
+  description = "Github OIDC Client ID"
+}
+variable "oidc_client_secret" {
+  description = "Github OIDC Client Secret"
+}
