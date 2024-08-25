d93b70cd50d7b9ac0f53bd1475ac2e43427e93a2
Loïc Herman <loic@loicherman.ch>
2024-08-25 21:01:14 +0200

    feat: authentication method for api via kubernetes

---
+++ applications/modules/vault/auth_kubernetes.tf
@@ -0,0 +1,21 @@
+resource "vault_auth_backend" "kubernetes" {
+  type = "kubernetes"
+}
+
+resource "vault_kubernetes_auth_backend_config" "presentium" {
+  backend = vault_auth_backend.kubernetes.path
+
+  kubernetes_host = "https://kubernetes.default.svc"
+}
+
+resource "vault_kubernetes_auth_backend_role" "presentium_api" {
+  backend = vault_auth_backend.kubernetes.path
+
+  role_name = "kubernetes"
+
+  bound_service_account_names      = ["presentium-api"]
+  bound_service_account_namespaces = ["staging-presentium", "prod-presentium"]
+
+  token_ttl      = 3600
+  token_policies = ["servers"]
+}
