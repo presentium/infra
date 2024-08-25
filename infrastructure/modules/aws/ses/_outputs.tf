2de80822779d7b90ba7b5f582d59194a99ee9f2f
Loïc Herman <loic@loicherman.ch>
2024-08-25 17:57:31 +0200

    fix: output smtp password for smtp users

--- infrastructure/modules/aws/ses/_outputs.tf
+++ infrastructure/modules/aws/ses/_outputs.tf
@@ -2,7 +2,7 @@ output "smtp_users" {
   value = {
     for key, val in aws_iam_access_key.smtp_user : key => {
       username = val.id
-      password = val.encrypted_secret
+      password = val.encrypted_ses_smtp_password_v4
     }
   }
 }
