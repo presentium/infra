f26ae85fa95ef74590ced809adaccf3da4101f78
Sacha Butty <sacha.butty@heig-vd.ch>
2024-08-29 13:11:55 +0200

    feat: set ebs storage class as default storage class

--- infrastructure/modules/aws/eks/k8s.tf
+++ infrastructure/modules/aws/eks/k8s.tf
@@ -1,6 +1,9 @@
 resource "kubernetes_storage_class_v1" "ebs" {
   metadata {
     name = "ebs"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" : "true"
+    }
   }
   storage_provisioner = "ebs.csi.aws.com"
   reclaim_policy      = "Retain"
